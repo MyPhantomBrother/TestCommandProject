1a575866c341d564b7bd4e342e8bf8ce4a2bb294
ArtGluhovskiy <gluhovskiy_art@mail.ru>
2018-03-17 17:37:00 +0300

    Bro:[Master]-Initial commit

---
+++ src/com/art/test/BroClass.java
@@ -0,0 +1,8 @@
+package com.art.test;
+
+public class BroClass {
+    public static void main(String[] args) {
+        //Bro initial commit
+        System.out.println("Hello From BroClass -> First commit");
+    }
+}
