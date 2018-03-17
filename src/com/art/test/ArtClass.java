efefd0ed08a817aa25a9387fa55647edc5a2762f
ArtGluhovskiy <gluhovskiy.art@gmail.com>
2018-03-17 19:00:22 +0300

    Art:[Master] - Initial commit

---
+++ src/com/art/test/ArtClass.java
@@ -0,0 +1,8 @@
+package com.art.test;
+
+public class ArtClass {
+    public static void main(String[] args) {
+        //Art:[Master] - Initial commit
+        System.out.println("Hello From ArtClass -> First commit.");
+    }
+}
