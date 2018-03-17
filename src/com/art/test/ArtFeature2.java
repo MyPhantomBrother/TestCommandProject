409476a726fe7103052ded75b07fea5d9b253b57
ArtGluhovskiy <gluhovskiy.art@gmail.com>
2018-03-17 22:46:30 +0300

    Art:[Art/feature2] - First commit

---
+++ src/com/art/test/ArtFeature2.java
@@ -0,0 +1,8 @@
+package com.art.test;
+
+public class ArtFeature2 {
+    public static void main(String[] args) {
+        //Art:[Art/feature2] - First commit
+        System.out.println("Art/feature2 - First commit");
+    }
+}
