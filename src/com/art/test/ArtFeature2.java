d80eba4fbd3bd69037cc7ca6a9f3765d8ba8d215
MyPhantomBrother <gluhovskiy_art@mail.ru>
2018-03-17 22:59:00 +0300

    Bro:[Master] - Cherry-picked from Art/feature2 branch commit

---
+++ src/com/art/test/ArtFeature2.java
@@ -0,0 +1,14 @@
+package com.art.test;
+
+public class ArtFeature2 {
+    public static void main(String[] args) {
+        //Art:[Art/feature2] - First commit
+        System.out.println("Art/feature2 - First commit");
+
+        //Art:[Art/feature2] - 2 commit
+        System.out.println("Art/feature2 - 2 commit");
+
+        //Art:[Art/feature2] - 3 commit
+        System.out.println("Art/feature2 - 3 commit");
+    }
+}
