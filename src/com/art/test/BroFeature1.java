4a5bed77c510b828b6a7417dac744355d1409753
MyPhantomBrother <gluhovskiy_art@mail.ru>
2018-03-17 22:40:41 +0300

    Bro:[Bro/feature-1] - 2 commit

--- src/com/art/test/BroFeature1.java
+++ src/com/art/test/BroFeature1.java
@@ -4,5 +4,8 @@ public class BroFeature1 {
     public static void main(String[] args) {
         //Bro:[Bro/feature-1] - First commit
         System.out.println("Bro/feature-1 - First commit");
+
+        //Bro:[Bro/feature-1] - 2 commit
+        System.out.println("Bro/feature-1 - 2 commit");
     }
 }
