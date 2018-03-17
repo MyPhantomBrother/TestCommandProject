63184377cc4344a6a87dd7f50812cc211d2dbdf5
MyPhantomBrother <gluhovskiy_art@mail.ru>
2018-03-17 22:40:18 +0300

    Bro:[Bro/feature-1] - First commit

---
+++ src/com/art/test/BroFeature1.java
@@ -0,0 +1,8 @@
+package com.art.test;
+
+public class BroFeature1 {
+    public static void main(String[] args) {
+        //Bro:[Bro/feature-1] - First commit
+        System.out.println("Bro/feature-1 - First commit");
+    }
+}
