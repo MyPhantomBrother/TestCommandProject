7544d51074c226d692e383c2d4828bdeac14e4f0
ArtGluhovskiy <gluhovskiy.art@gmail.com>
2018-03-17 22:46:51 +0300

    Art:[Art/feature2] - 2 commit

--- src/com/art/test/ArtFeature2.java
+++ src/com/art/test/ArtFeature2.java
@@ -4,5 +4,8 @@ public class ArtFeature2 {
     public static void main(String[] args) {
         //Art:[Art/feature2] - First commit
         System.out.println("Art/feature2 - First commit");
+
+        //Art:[Art/feature2] - 2 commit
+        System.out.println("Art/feature2 - 2 commit");
     }
 }
